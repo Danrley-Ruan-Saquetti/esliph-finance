77a3e74b5b877ae9b37fb3cfae264eab077f82f1
Dan Ruan <danrsaquetti@gmail.com>
2023-09-16 14:07:33 -0300

    D.

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native'
 
 export default function App() {
     return (
-        <View className="bg-gray-600 flex-1 justify-center items-center">
-            <Text className="text-zinc-50 text-xl">Teste!</Text>
+        <View className="bg-gray-800 flex-1 justify-center items-center">
+            <Text className="text-zinc-50 text-xl">Hello World!</Text>
             <StatusBar />
         </View>
     )
--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./App.tsx', './src/**/*.tsx'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ['./App.tsx', './app/**/*.tsx'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
