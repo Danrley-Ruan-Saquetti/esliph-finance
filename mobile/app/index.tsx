7e486a272a240d77c92792d1ee0170ca9d2d7ac9
Dan Ruan <danrsaquetti@gmail.com>
2023-09-24 15:09:19 -0300

    path alias config complete

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,14 +1,11 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
-import { Link } from 'expo-router'
 
 export default function App() {
     return (
         <View className="bg-gray-800 flex-1 justify-center items-center">
             <Text className="text-zinc-50 text-xl">Hello World!</Text>
-            <Link href="/users">View users</Link>
             <StatusBar />
         </View>
     )
 }
-
--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,6 +2,22 @@ module.exports = function (api) {
     api.cache(true)
     return {
         presets: ['babel-preset-expo'],
-        plugins: ['nativewind/babel', 'expo-router/babel'],
+        plugins: [
+            'nativewind/babel',
+            'expo-router/babel', [
+                'module-resolver',
+                {
+                    root: '.',
+                    alias: {
+                        '@router': './app',
+                        '@assets': './assets',
+                        '@server': './server',
+                        '@components': './components',
+                        '@services': './services',
+                        '@util': './util',
+                    }
+                }
+            ]
+        ],
     }
 }
