6ecbf4b10261c055f314f38698c07583e605dbb5
Dan Ruan <danrsaquetti@gmail.com>
2023-09-30 16:34:58 -0300

    update mobile

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,22 +2,6 @@ module.exports = function (api) {
     api.cache(true)
     return {
         presets: ['babel-preset-expo'],
-        plugins: [
-            'nativewind/babel',
-            'expo-router/babel', [
-                'module-resolver',
-                {
-                    root: '.',
-                    alias: {
-                        '@router': './app',
-                        '@assets': './assets',
-                        '@server': './server',
-                        '@components': './components',
-                        '@services': './services',
-                        '@util': './util',
-                    }
-                }
-            ]
-        ],
+        plugins: ['nativewind/babel', 'expo-router/babel'],
     }
 }
---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+
+export default function App() {
+    return (
+        <View className="flex-1 items-center justify-center bg-zinc-800">
+            <Text>Hello World</Text>
+        </View>
+    )
+}
+
