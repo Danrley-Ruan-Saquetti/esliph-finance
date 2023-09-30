67eeb844776c3de1e61bf018d0a509e5b43d7aaf
Dan Ruan <danrsaquetti@gmail.com>
2023-09-30 17:32:46 -0300

    add path alias

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,6 +2,19 @@ module.exports = function (api) {
     api.cache(true)
     return {
         presets: ['babel-preset-expo'],
-        plugins: ['nativewind/babel', 'expo-router/babel'],
+        plugins: ['nativewind/babel', 'expo-router/babel', [
+            'module-resolver',
+            {
+                root: ['.'],
+                alias: {
+                    '@router': './src/app',
+                    '@assets': './src/assets',
+                    '@server': './src/server',
+                    '@components': './src/components',
+                    '@services': './src/services',
+                    '@util': './src/util',
+                }
+            }
+        ]],
     }
 }
--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,9 +1,10 @@
 import { Text, View } from 'react-native'
+import { hello } from '@server/app'
 
 export default function App() {
     return (
-        <View className="flex-1 items-center justify-center bg-zinc-800">
-            <Text>Hello World</Text>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text>Hello World {hello}</Text>
         </View>
     )
 }
