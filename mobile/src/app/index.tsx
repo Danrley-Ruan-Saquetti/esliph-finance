b3cc2c3355ffec984351540ee67af3e44f41e28c
Dan Ruan <danrsaquetti@gmail.com>
2023-10-01 14:37:19 -0300

    ref(server): transfer server folder to mobile

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,11 +1,9 @@
 import { Text, View } from 'react-native'
-import { hello } from '@server/app'
 
 export default function App() {
     return (
         <View className="flex-1 items-center justify-center bg-white">
-            <Text>Hello World {hello}</Text>
+            <Text>Hello World</Text>
         </View>
     )
 }
-
