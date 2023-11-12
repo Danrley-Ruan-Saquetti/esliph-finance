ffdf5177d4cac4692166b9c4285c0ea53f1c2580
Dan Ruan <danrsaquetti@gmail.com>
2023-11-12 18:48:23 -0300

    add services

--- mobile/src/server/main.module.ts
+++ mobile/src/server/main.module.ts
@@ -1,7 +1,15 @@
 import { AppModule } from '@server/app/app.module'
 import { Module } from '@esliph/module'
+import { Logger } from './services/logger.service'
+import { JWTService } from './services/jwt.service'
 
 @Module({
-    imports: [AppModule]
+    imports: [AppModule],
+    providers: [
+        Logger,
+        JWTService,
+        { whenCall: 'logger', use: 'global.service.logger' },
+        { whenCall: 'jwt', use: 'global.service.jwt' }
+    ]
 })
 export class MainModule { }
