db71567bd01b05d3cdac5ac2363c15b87329f4b7
Dan Ruan <danrsaquetti@gmail.com>
2023-11-20 21:14:19 -0300

    .

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -2,29 +2,9 @@ import { Text, View } from 'react-native'
 import { Bootstrap } from '@esliph/module'
 import { Injection } from '@esliph/injection'
 import { MainModule } from '@server/main.module'
-import { JWTService } from '@server/services/jwt.service'
 
 Injection.Clear()
 Bootstrap(MainModule, { logLoad: true, logEventHttp: true, logEventListener: true })
-
-async function Boot() {
-    const jwtService = Injection.resolve(JWTService)
-
-    const secret = 'meu-segredo'
-    const payload = {
-        sub: 1,
-    }
-
-    const token = await jwtService.encode(payload, { secret, exp: 60 * 60 * 24 })
-
-    console.log(token)
-
-    const decode = await jwtService.decode(token, secret)
-
-    console.log(decode)
-}
-
-Boot()
 
 export default function App() {
     return (
