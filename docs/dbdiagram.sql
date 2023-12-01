75dabc4a2121167ed1ff46bd605c8097d54a58e8
Dan Ruan <101991292+Danrley-Ruan-Saquetti@users.noreply.github.com>
2023-12-01 14:46:08 +0000

    update db

--- docs/dbdiagram.sql
+++ docs/dbdiagram.sql
@@ -2,117 +2,104 @@
 # Docs: https://dbml.dbdiagram.io/docs
 # https://dbdiagram.io/d/portal-finance-656716f83be1495787f68936
 
-Table account {
-  id int [primary key]
-  name varchar(45) [not null]
-  email varchar(25) [not null, unique]
-  password varchar(15) [not null]
-  balance double [default: 0]
-  created_at timestamp [default: `now()`]
-  updated_at timestamp [default: `now()`]
-}
+CREATE TABLE "account" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "name" TEXT NOT NULL,
+  "email" TEXT UNIQUE NOT NULL,
+  "password" TEXT NOT NULL,
+  "balance" REAL DEFAULT 0,
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-enum notification_type {
-  Push
-  Internal
-  Mail
-}
+CREATE TABLE "notification" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "account_id" INTEGER NOT NULL,
+  "subject" TEXT NOT NULL,
+  "content" TEXT NOT NULL,
+  "was_read" INTEGER DEFAULT 0,
+  "type" TEXT NOT NULL,
+  "is_send" INTEGER DEFAULT 0,
+  "send_at" TIMESTAMP,
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY ("account_id") REFERENCES "account" ("id")
+);
 
-Table notification {
-  id int [primary key]
-  account_id int [not null, ref: > account.id]
-  subject varchar(45) [not null]
-  content text [not null]
-  was_read boolean [default: false]
-  type notification_type [not null]
-  is_send boolean [default: false]
-  send_at timestamp
-  created_at timestamp [default: `now()`]
-  updated_at timestamp [default: `now()`]
-}
+CREATE TABLE "mail" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "notification_id" INTEGER NOT NULL,
+  "recipient" TEXT NOT NULL,
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY ("notification_id") REFERENCES "notification" ("id")
+);
 
-Table mail {
-  id int [primary key]
-  notification_id int [not null, ref: > notification.id]
-  recipient varchar(45) [not null]
-  created_at timestamp [default: `now()`]
-  updated_at timestamp [default: `now()`]
-}
+CREATE TABLE "category" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "account_id" INTEGER NOT NULL,
+  "name" TEXT NOT NULL,
+  "color" TEXT NOT NULL,
+  "is_favorite" INTEGER DEFAULT 0,
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY ("account_id") REFERENCES "account" ("id")
+);
 
-Table category {
-  id int [primary key]
-  account_id int [not null, ref: > account.id]
-  name varchar(45) [not null]
-  color varchar(15) [not null]
-  is_favorite boolean [default: false]
-  created_at timestamp [default: `now()`]
-  updated_at timestamp [default: `now()`]
-}
+CREATE TABLE "financial_transaction" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "account_id" INTEGER NOT NULL,
+  "title" TEXT NOT NULL,
+  "description" TEXT,
+  "value" REAL NOT NULL,
+  "priority" INTEGER,
+  "is_observable" INTEGER DEFAULT 0,
+  "is_send_notification" INTEGER DEFAULT 0,
+  "times_to_repeat" INTEGER DEFAULT 0,
+  "count_repeated_occurrences" INTEGER DEFAULT 0,
+  "type" TEXT NOT NULL,
+  "receiver" TEXT,
+  "sender" TEXT,
+  "type_occurrence" TEXT DEFAULT 'Single',
+  "situation" TEXT DEFAULT 'Pending',
+  "expires_in" TIMESTAMP NOT NULL,
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY ("account_id") REFERENCES "account" ("id")
+);
 
-enum financial_transaction_type {
-  Expense
-  Income
-}
+CREATE TABLE "note" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "financial_transaction_id" INTEGER NOT NULL,
+  "description" TEXT,
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY ("financial_transaction_id") REFERENCES "financial_transaction" ("id")
+);
 
-enum financial_transaction_occurrence {
-  Single
-  Programmatic
-}
+CREATE TABLE "payment" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "financial_transaction_id" INTEGER NOT NULL,
+  "value" REAL NOT NULL,
+  "discount" REAL DEFAULT 0,
+  "increase" REAL DEFAULT 0,
+  "paid_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY ("financial_transaction_id") REFERENCES "financial_transaction" ("id")
+);
 
-enum financial_transaction_situation {
-  Pending
-  PaidOut
-  PartiallyPaid
-  Received
-  PartiallyReceived
-  Late
-  Canceled
-}
+CREATE TABLE "media" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "financial_transaction_id" INTEGER NOT NULL,
+  "name" TEXT,
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY ("financial_transaction_id") REFERENCES "financial_transaction" ("id")
+);
 
-Table financial_transaction {
-  id int [primary key]
-  account_id int [not null, ref: > account.id]
-  title varchar(55) [not null]
-  description text
-  value double [not null]
-  priority int
-  is_observable boolean [default: false]
-  is_send_notification boolean [default: false]
-  times_to_repeat int [default: 0]
-  count_repeated_occurrences int [default: 0]
-  type financial_transaction_type [not null]
-  receiver string
-  sender string
-  type_occurrence financial_transaction_occurrence [default: 'Single']
-  situation financial_transaction_situation [default: 'Pending']
-  expires_in timestamp [not null]
-  created_at timestamp [default: `now()`]
-  updated_at timestamp [default: `now()`]
-}
-
-Ref financeial_transaction_category: financial_transaction.id <> category.id // many-to-many
-
-Table note {
-  id int [primary key]
-  financial_transaction_id int [not null, ref: > financial_transaction.id]
-  description text
-  created_at timestamp [default: `now()`]
-  updated_at timestamp [default: `now()`]
-}
-
-Table payment {
-  id int [primary key]
-  financial_transaction_id int [not null, ref: > financial_transaction.id]
-  value double [not null]
-  discount double [default: 0]
-  increase double [default: 0]
-  paid_at timestamp [default: `now()`]
-  created_at timestamp [default: `now()`]
-}
-
-Table midia {
-  id int [primary key]
-  financial_transaction_id int [not null, ref: > financial_transaction.id]
-  name varchar(75)
-  created_at timestamp [default: `now()`]
-}
+CREATE TABLE "financial_transaction_category" (
+  "financial_transaction_id" INTEGER,
+  "category_id" INTEGER,
+  PRIMARY KEY ("financial_transaction_id", "category_id"),
+  FOREIGN KEY ("financial_transaction_id") REFERENCES "financial_transaction" ("id"),
+  FOREIGN KEY ("category_id") REFERENCES "category" ("id")
+);
