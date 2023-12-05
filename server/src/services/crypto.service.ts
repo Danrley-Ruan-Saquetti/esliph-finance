594f43beeb019341cd45dbdaf39f3681666b0126
Dan Ruan <101991292+Danrley-Ruan-Saquetti@users.noreply.github.com>
2023-12-05 20:38:03 +0000

    fix name service global crypto

--- server/src/services/crypto.service.ts
+++ server/src/services/crypto.service.ts
@@ -2,7 +2,7 @@ import { Service } from '@esliph/module'
 import crypto from 'node:crypto'
 import bcrypto from 'bcrypt'
 
-@Service({ name: 'global.service.crytpo' })
+@Service({ name: 'global.service.crypto' })
 export class CryptoService {
     crypto = crypto
     bcrypto = bcrypto
