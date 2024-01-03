4a222b7cb39e65c6ff9ac359031e10b1141e2d69
Dan Ruan <danrsaquetti@gmail.com>
2024-01-03 11:01:49 -0300

    update job service

--- server/src/services/job.service.ts
+++ server/src/services/job.service.ts
@@ -1,7 +1,9 @@
 import { Service, ApplicationModule } from '@esliph/module'
 import { JobOptions, Job as EsliphJob, CronOptions, Cron as EsliphCron, Bootstrap as JobBootstrap, isJob } from '@esliph/job'
-import { isInstance } from '@util'
 import { ClassConstructor } from '@@types'
+import { isInstance } from '@util'
+import { GLOBAL_LOG_CONFIG } from '@global'
+import { WriteStreamOutput } from '@services/write-stream-output.service'
 export * from '@esliph/job'
 
 export function Job(options: JobOptions) {
@@ -22,8 +24,22 @@ export function Cron(options: CronOptions) {
 
 @Service({ name: 'global.service.job' })
 export class JobService {
+    constructor() { }
+
     static onStart() {
+        const writer = WriteStreamOutput.newInstance(`${GLOBAL_LOG_CONFIG.path}/job.log`)
+
         const jobsProviders = ApplicationModule.getProviders().filter(provider => isInstance(provider)).filter(provider => isJob(provider)) as ClassConstructor[]
+
+        ApplicationModule.listener.on('job/start', data => {
+            writer.write(`Job "${data.name}" start`)
+        })
+        ApplicationModule.listener.on('job/end', data => {
+            writer.write(`Job "${data.name}" end`)
+        })
+        ApplicationModule.listener.on('job/erro', data => {
+            writer.write(`Job "${data.name}" erro`)
+        })
 
         JobBootstrap(jobsProviders)
     }
