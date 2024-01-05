fb1ea8922499d81b52cdac29762c38fe03b9dbea
Dan Ruan <danrsaquetti@gmail.com>
2024-01-05 16:38:01 -0300

    add auth-bank-account-module in auth-module

--- server/src/app/modules/auth/auth.module.ts
+++ server/src/app/modules/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@esliph/module'
 import { AuthController } from '@modules/auth/auth.controller'
 import { AuthUserModule } from '@modules/auth/user/auth-user.module'
+import { AuthBankAccountModule } from '@modules/auth/bank-account/auth-bank-account.module'
 
 @Module({
-    imports: [AuthUserModule],
+    imports: [AuthUserModule, AuthBankAccountModule],
     controllers: [AuthController],
 })
 export class AuthModule { }
