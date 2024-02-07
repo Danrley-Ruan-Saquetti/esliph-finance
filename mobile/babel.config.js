f3ac8c3aeffeee8d1cfeeb049d70c3b39d7bd924
Dan Ruan <danrsaquetti@gmail.com>
2024-02-07 15:42:29 -0300

    init mobile project

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,20 +1,6 @@
 module.exports = function (api) {
     api.cache(true)
     return {
-        presets: ['babel-preset-expo'],
-        plugins: [
-            'expo-router/babel',
-            'nativewind/babel', [
-                'module-resolver',
-                {
-                    root: ['.'],
-                    alias: {
-                        '@router': './src/app',
-                        '@': './src',
-                    },
-                },
-            ],
-        ],
-        ignore: ['node_modules', 'scripts'],
+        presets: ['babel-preset-expo']
     }
 }
--- mobile/scripts/setup.sh
+++
@@ -1,7 +0,0 @@
-rm -r node_modules package-lock.json --force
-
-npm i
-
-npm i -D tailwindcss@3.3.2
-
-npm run expo:update
