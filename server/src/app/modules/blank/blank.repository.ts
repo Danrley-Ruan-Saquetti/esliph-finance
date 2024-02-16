0075cd47c7dd55d4cf3034e0e36ba3353f5d2b4a
Dan Ruan <danrsaquetti@gmail.com>
2024-02-16 16:15:58 -0300

    update repository blank: add query and count function

--- server/src/app/modules/blank/blank.repository.ts
+++ server/src/app/modules/blank/blank.repository.ts
@@ -1,5 +1,6 @@
 import { Prisma } from '@prisma/client'
 import { Service } from '@core'
+import { MetadataQuery } from '@@types'
 import { Repository } from '@services/repository.service'
 
 type BlankGetPayloadTypes = boolean | null | undefined | { select?: Prisma.BlankSelect | null }
@@ -33,6 +34,10 @@ export class BlankRepository extends Repository {
         findMany: {
             title: 'Find Blanks',
             failed: 'Unable to query blanks'
+        },
+        count: {
+            title: 'Count Financial Transactions',
+            failed: 'Unable to count financial transactions'
         }
     }
 
@@ -102,17 +107,68 @@ export class BlankRepository extends Repository {
         }
     }
 
+    async query<Args extends Prisma.BlankFindManyArgs>(args: Args, page: { pageIndex: number, limite: number }) {
+        const totalResult = await this.count({
+            where: { ...args.where }
+        })
+
+        if (!totalResult.isSuccess()) {
+            return this.handleError<{ blanks: BlankFindResponse<Args>[], metadata: MetadataQuery }>(totalResult.getError(), {
+                error: {
+                    title: BlankRepository.GLOBAL_MESSAGE.findMany.title,
+                    message: BlankRepository.GLOBAL_MESSAGE.findMany.failed
+                }
+            })
+        }
+
+        const blanksResult = await this.findMany({
+            ...args,
+            skip: page.pageIndex * page.limite,
+            take: page.limite
+        })
+
+        if (!blanksResult.isSuccess()) {
+            return this.handleError<{ blanks: BlankFindResponse<Args>[], metadata: MetadataQuery }>(totalResult.getError(), {
+                error: {
+                    title: BlankRepository.GLOBAL_MESSAGE.findMany.title,
+                    message: BlankRepository.GLOBAL_MESSAGE.findMany.failed
+                }
+            })
+        }
+
+        const result = {
+            blanks: blanksResult.getValue() || [],
+            metadata: {
+                currentPage: page.pageIndex,
+                itemsPerPage: page.limite,
+                totalOfItens: totalResult.getValue(),
+                totalOfPages: Math.ceil(totalResult.getValue() / page.limite),
+            }
+        }
+
+        return this.handleResponse<{ blanks: BlankFindResponse<Args>[], metadata: MetadataQuery }>(result as any)
+    }
+
     async findMany<Args extends Prisma.BlankFindManyArgs>(args: Args) {
         try {
-            const blank = await this.database.instance.blank.findMany(args) as BlankFindResponse<Args>[]
+            const blanks = await this.database.instance.blank.findMany(args) as BlankFindResponse<Args>[]
 
-            return this.handleResponse<BlankFindResponse<Args>[]>(blank, {
-                noAcceptNullable: true,
-                error: { title: BlankRepository.GLOBAL_MESSAGE.find.title, message: BlankRepository.GLOBAL_MESSAGE.find.notFound },
-            })
+            return this.handleResponse<BlankFindResponse<Args>[]>(blanks)
         } catch (err: any) {
             return this.handleError<BlankFindResponse<Args>[]>(err, {
-                error: { title: BlankRepository.GLOBAL_MESSAGE.find.title, message: BlankRepository.GLOBAL_MESSAGE.find.failed }
+                error: { title: BlankRepository.GLOBAL_MESSAGE.findMany.title, message: BlankRepository.GLOBAL_MESSAGE.findMany.failed }
+            })
+        }
+    }
+
+    async count(args: Prisma.BlankCountArgs) {
+        try {
+            const blank = await this.database.instance.blank.count(args)
+
+            return this.handleResponse<number>(blank)
+        } catch (err: any) {
+            return this.handleError<number>(err, {
+                error: { title: BlankRepository.GLOBAL_MESSAGE.findMany.title, message: BlankRepository.GLOBAL_MESSAGE.findMany.failed }
             })
         }
     }
